877a54c10dbe92b5f0638bacf26163214e0dffc9
vault4887 <vault4887@gmail.com>
2026-02-13 11:30:59 -0800

    Update location.js

--- netlify/functions/location.js
+++ netlify/functions/location.js
@@ -1,32 +1,21 @@
 exports.handler = async function(event) {
-  try {
-    const ip =
-      event.headers["x-nf-client-connection-ip"] ||
-      event.headers["client-ip"] ||
-      event.headers["x-forwarded-for"]?.split(",")[0] ||
-      "Unknown";
+  const ip =
+    event.headers["x-nf-client-connection-ip"] ||
+    event.headers["client-ip"] ||
+    event.headers["x-forwarded-for"]?.split(",")[0] ||
+    "Unknown";
 
-    const response = await fetch(`https://ipapi.co/${ip}/json/`);
-    const data = await response.json();
+  const country = event.headers["x-country"] || "Unknown";
+  const region = event.headers["x-region"] || "Unknown";
+  const city = event.headers["x-city"] || "Unknown";
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        ip: ip,
-        city: data.city || "Unknown",
-        region: data.region || "Unknown",
-        country: data.country_name || "Unknown"
-      })
-    };
-  } catch (error) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        ip: "Unknown",
-        city: "Unknown",
-        region: "Unknown",
-        country: "Unknown"
-      })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      ip: ip,
+      city: city,
+      region: region,
+      country: country
+    })
+  };
 };
