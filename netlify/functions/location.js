3520e08fcb6a0bc5268d0743d8aee3a68161335a
vault4887 <vault4887@gmail.com>
2026-02-13 11:23:33 -0800

    Add files via upload

---
+++ netlify/functions/location.js
@@ -0,0 +1,29 @@
+
+exports.handler = async function(event) {
+  try {
+    const ip = event.headers['x-forwarded-for']?.split(',')[0] || event.headers['client-ip'];
+
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ip: ip || "Unknown",
+        city: data.city || "Unknown",
+        region: data.region || "Unknown",
+        country: data.country_name || "Unknown"
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ip: "Unknown",
+        city: "Unknown",
+        region: "Unknown",
+        country: "Unknown"
+      })
+    };
+  }
+};
